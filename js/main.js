1ed1aa6e35c54e6f848b9ca9be17c17d01a7ea1d
JJW37 <johnwilliamwallace@protonmail.com>
2026-08-04 20:39:09 -0700

    Update CSS version in main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
   var style = document.createElement('link');
   style.rel = 'stylesheet';
-  style.href = '/chilterngardenmaintenance-updatedsite/css/premium-experience.css?v=20260805p2';
+  style.href = '/chilterngardenmaintenance-updatedsite/css/premium-experience.css?v=20260805p3';
   document.head.appendChild(style);
 
   var script = document.createElement('script');
